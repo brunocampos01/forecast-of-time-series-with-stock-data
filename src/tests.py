56a4bb190b7a38b621458716f52c3c3f1a807346
brunocampos01 <brunocampos01@gmail.com>
2020-11-27 05:32:28 -0300

    validação da pesquisa: criar notebooks com testes preliminares

---
+++ src/tests.py
@@ -0,0 +1,24 @@
+# Data analysis and data wrangling
+import numpy as np
+import pandas as pd
+
+# Metrics
+import statistics
+
+# Plotting
+import seaborn as sns
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_pacf
+
+# statsmodels
+import statsmodels.api as sm
+from statsmodels.tsa.arima_model import ARIMA
+
+
+# Other
+from IPython.display import Image
+import warnings
+import pprint
+import datetime
+import os
